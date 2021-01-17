ccacc09dec871d0997f5897b13870021686baab6
qverkk <membersy@gmail.com>
2021-01-17 21:42:17 +0100

    Fix cookie authorization

--- src/main/kotlin/com/qverkk/orgella/zuul/security/AuthorizationFilter.kt
+++ src/main/kotlin/com/qverkk/orgella/zuul/security/AuthorizationFilter.kt
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 import javax.servlet.FilterChain
+import javax.servlet.http.Cookie
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -16,9 +17,10 @@ class AuthorizationFilter(
 ) : BasicAuthenticationFilter(authenticationManager) {
 
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
-        val authorizationHeader: String? = request.getHeader(env.getProperty("authorization.token.header.name"))
+        val cookies: Array<out Cookie>? = request.cookies
+        val cookie: Cookie? = cookies?.first { it.name == "UserInfo" }
 
-        if (authorizationHeader == null || !authorizationHeader.startsWith(env.getProperty("authorization.token.header.prefix")!!)) {
+        if (cookie == null) {
             chain.doFilter(request, response)
             return
         }
@@ -30,14 +32,10 @@ class AuthorizationFilter(
     }
 
     private fun getAuthentication(request: HttpServletRequest): UsernamePasswordAuthenticationToken? {
-        val authorizationHeader: String = request.getHeader(env.getProperty("authorization.token.header.name"))
-            ?: return null
-
-        val token = authorizationHeader.replace(env.getProperty("authorization.token.header.prefix")!!, "")
-
+        val cookie: Cookie = request.cookies.first { it.name == "UserInfo" }
         val userId = Jwts.parser()
             .setSigningKey(env.getProperty("token.secret"))
-            .parseClaimsJws(token)
+            .parseClaimsJws(cookie.value)
             .body
             .subject ?: return null
 
