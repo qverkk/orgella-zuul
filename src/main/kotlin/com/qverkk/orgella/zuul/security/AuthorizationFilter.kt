42eb527150ebb26b2a261708a5e553ea5fa65ce7
qverkk <membersy@gmail.com>
2021-01-18 18:33:34 +0100

    Fix parsing cookies in authorization

--- src/main/kotlin/com/qverkk/orgella/zuul/security/AuthorizationFilter.kt
+++ src/main/kotlin/com/qverkk/orgella/zuul/security/AuthorizationFilter.kt
@@ -4,8 +4,10 @@ import io.jsonwebtoken.Jwts
 import org.springframework.core.env.Environment
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import java.util.stream.Collectors
 import javax.servlet.FilterChain
 import javax.servlet.http.Cookie
 import javax.servlet.http.HttpServletRequest
@@ -18,8 +20,12 @@ class AuthorizationFilter(
 
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
         val cookies: Array<out Cookie>? = request.cookies
-        val cookie: Cookie? = cookies?.first { it.name == "UserInfo" }
-
+//        val cookie: Cookie? = cookies?.first { it.name == "UserInfo" }
+        if (cookies == null) {
+            chain.doFilter(request, response)
+            return
+        }
+        val cookie = cookies.toList().stream().filter { it.name == "UserInfo" }.findFirst().orElseGet { null }
         if (cookie == null) {
             chain.doFilter(request, response)
             return
@@ -32,13 +38,24 @@ class AuthorizationFilter(
     }
 
     private fun getAuthentication(request: HttpServletRequest): UsernamePasswordAuthenticationToken? {
-        val cookie: Cookie = request.cookies.first { it.name == "UserInfo" }
-        val userId = Jwts.parser()
+//        val cookie: Cookie = request.cookies?.first { it.name == "UserInfo" } ?: return null
+        val cookie = request.cookies.toList().stream().filter { it.name == "UserInfo" }.findFirst().orElseGet { null }
+
+        val jwtBody = Jwts.parser()
             .setSigningKey(env.getProperty("token.secret"))
             .parseClaimsJws(cookie.value)
             .body
-            .subject ?: return null
 
-        return UsernamePasswordAuthenticationToken(userId, null, arrayListOf())
+        val userId = jwtBody.subject ?: return null
+
+        val roles = (jwtBody["roles"] as String)
+            .replace("[", "")
+            .replace("]", "")
+            .split(",")
+            .stream()
+            .map { SimpleGrantedAuthority(it) }
+            .collect(Collectors.toList())
+
+        return UsernamePasswordAuthenticationToken(userId, null, roles)
     }
 }
